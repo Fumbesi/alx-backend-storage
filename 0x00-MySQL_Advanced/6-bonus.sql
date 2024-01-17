b4811a97d68cf5e4bf539d232ce89c975ac405d4
Khwezi Ndlovu <khwezindlovu25@gmail.com>
2024-01-17 09:06:23 -0800

    006

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,22 @@
+-- 6-bonus.sql
+-- Script to create a stored procedure AddBonus
+
+DELIMITER //
+
+CREATE PROCEDURE AddBonus(IN user_id_param INT, IN project_name_param VARCHAR(255), IN score_param INT)
+BEGIN
+    DECLARE project_id_param INT;
+
+    -- Check if the project exists, otherwise create it
+    SELECT id INTO project_id_param FROM projects WHERE name = project_name_param;
+    
+    IF project_id_param IS NULL THEN
+        INSERT INTO projects (name) VALUES (project_name_param);
+        SET project_id_param = LAST_INSERT_ID();
+    END IF;
+
+    -- Add the bonus correction
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id_param, project_id_param, score_param);
+END //
+
+DELIMITER ;

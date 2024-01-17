8a324bd27e51ad305bf6f155413f3af9253e5938
Khwezi Ndlovu <khwezindlovu25@gmail.com>
2024-01-17 09:11:49 -0800

    008

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,8 +1,5 @@
 -- 8-index_my_names.sql
 -- Script to create an index idx_name_first on the table names and the first letter of name
 
--- Import the table dump
-SOURCE /path/to/names.sql;
-
 -- Create the index
 CREATE INDEX idx_name_first ON names (LEFT(name, 1));

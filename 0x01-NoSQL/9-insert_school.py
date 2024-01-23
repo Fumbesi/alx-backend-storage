0af017749725f2f3222d8bf3c991bb02dc8a4c89
Khwezi Ndlovu <khwezindlovu25@gmail.com>
2024-01-23 10:30:40 -0800

    09

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3
-"""
-9-insert_school module
-"""
+"""Python function that inserts a new document in a collection based on kwargs"""
 
 from pymongo.collection import Collection
-from typing import Any
 
-def insert_school(mongo_collection: Collection, **kwargs: Any) -> Any:
+def insert_school(mongo_collection: Collection, **kwargs) -> str:
     """
-    Inserts a new document in the specified MongoDB collection based on kwargs.
+    Inserts a new document in the MongoDB collection based on the provided kwargs.
 
-    Args:
-        mongo_collection (pymongo.collection.Collection): The pymongo collection object.
-        **kwargs: Keyword arguments representing the fields for the new document.
-
-    Returns:
-        Any: The new _id of the inserted document.
+    :param mongo_collection: The `mongo_collection` parameter is expected
+    to be a MongoDB collection object.
+    It represents a collection of documents in a MongoDB database.
+    :param kwargs: Key-value pairs representing the fields and values of the new document.
+    :return: The new _id of the inserted document.
     """
-    result = mongo_collection.insert_one(kwargs)
-    return result.inserted_id
+    new_document = kwargs
+    result = mongo_collection.insert_one(new_document)
+    return str(result.inserted_id)
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    school_collection = client.my_db.school
+    new_school_id = insert_school(school_collection, name="UCSF", address="505 Parnassus Ave")
+    print("New school created: {}".format(new_school_id))
+
+    schools = list_all(school_collection)
+    for school in schools:
+        print("[{}] {} {}".format(school.get('_id'), school.get('name'), school.get('address', "")))
